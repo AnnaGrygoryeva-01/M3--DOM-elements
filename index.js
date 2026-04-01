2f5712a5f154dc51fdd51c405eeedb34b4c44a51
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2026-04-01 15:04:47 +0100

    Complete task-3

--- index.js
+++ index.js
@@ -3,3 +3,36 @@
 // 2. За допомогою DOM отримати посилання на існуючий на сторінці елемент (div або section, який заздалегідь має існувати в розмітці і містити якийсь тексту) та змінити йому колір тла на фіолетовий, а колір тексту на білий.
 
 // 3. За допомогою prompt отримати у користувача його ім’я, після чого створити і відобразити на сторінці елемент <h1>, який містить текст “Вітаю, (введене користувачем ім’я)”
+
+const greeting = document.createElement("h1");
+
+function setGreeting() {
+  while (true) {
+    const userName = prompt("Hello, what is your name?");
+
+    try {
+      if (userName === null) {
+        throw new Error("Operation cancelled by user.");
+      }
+
+      if (userName.trim().length === 0) {
+        throw new Error("Name cannot be empty. Please try again.");
+      }
+
+      if (!isNaN(userName)) {
+        throw new TypeError("Name cannot be a number! Please use letters.");
+      }
+
+      return `Welcome, ${userName}, would you like to find out more about our services?`;
+    } catch (error) {
+      alert(`${error.name}: ${error.message}`);
+
+      if (error.message === "Operation cancelled by user.") {
+        return "Welcome, would you like to find out more about our services?";
+      }
+    }
+  }
+}
+
+greeting.textContent = setGreeting();
+document.body.prepend(greeting);
